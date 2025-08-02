132b097a3c6e5f5e7cc05ba80df123d67384606f
Tann2019 <103302160+Tann2019@users.noreply.github.com>
2025-08-02 16:30:05 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,8 @@
 <body class="bg-gradient-to-br from-slate-900 via-blue-900 to-indigo-900 min-h-screen text-slate-100 overflow-x-hidden">
     <!-- Background Pattern -->
     <div class="absolute inset-0 bg-gradient-to-r from-blue-600/10 to-purple-600/10"></div>
-    <div class="absolute inset-0" style="background-image: url('data:image/svg+xml,<svg width="60" height="60"
-        viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
-        <g fill="none" fill-rule="evenodd">
-            <g fill="%23ffffff" fill-opacity="0.05">
-                <circle cx="30" cy="30" r="2" />
-            </g>
-        </g></svg>');">
+    <div class="absolute inset-0"
+        style="background-image: url('data:image/svg+xml,%3Csvg width=%2260%22 height=%2260%22 viewBox=%220 0 60 60%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cg fill=%22none%22 fill-rule=%22evenodd%22%3E%3Cg fill=%22%23ffffff%22 fill-opacity=%220.05%22%3E%3Ccircle cx=%2230%22 cy=%2230%22 r=%222%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');">
     </div>
 
     <!-- Navigation -->
